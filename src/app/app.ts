3aef407e6bd364748ada5375e2be96e5332a5999
JorgePizarroL <jpizarrol1@est.ups.edu.ec>
2026-06-04 01:27:53 -0500

    Portafolio

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('frontend');
+export class App implements OnInit {
+  programadores = signal<any[]>([]);
+  proyectos = signal<any[]>([]);
+  servicios = signal<any[]>([]);
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<any>('http://localhost:1337/api/programadors?populate=foto').subscribe(res => {
+      this.programadores.set(res.data);
+    });
+
+    this.http.get<any>('http://localhost:1337/api/proyectos?filters[destacado][$eq]=true&populate=programadors').subscribe(res => {
+      this.proyectos.set(res.data);
+    });
+
+    this.http.get<any>('http://localhost:1337/api/servicios').subscribe(res => {
+      this.servicios.set(res.data);
+    });
+  }
 }
